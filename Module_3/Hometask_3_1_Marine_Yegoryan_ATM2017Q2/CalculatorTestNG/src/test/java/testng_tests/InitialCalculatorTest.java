dd3a2cfb19eea0914dde3a868859a03ab3c1de67
marineyegoryan@gmail.com <marine_yegoryan@epam.com>
2017-06-26 15:04:35 +0400

    updated .xml configuration files and InitialCalculatorTest class

--- Module_3/Hometask_3_1_Marine_Yegoryan_ATM2017Q2/CalculatorTestNG/src/test/java/testng_tests/InitialCalculatorTest.java
+++ Module_3/Hometask_3_1_Marine_Yegoryan_ATM2017Q2/CalculatorTestNG/src/test/java/testng_tests/InitialCalculatorTest.java
@@ -1,8 +1,7 @@
 package testng_tests;
 
 import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-
+import org.testng.annotations.BeforeTest;
 
 import com.epam.tat.module4.Calculator;
 
@@ -10,7 +9,7 @@ public class InitialCalculatorTest {
 
 	protected Calculator calculator;
 	
-	@BeforeClass()
+	@BeforeTest()
 	public void setUp(){
 		System.out.println("Test Case two with Thread Id:-> " + Thread.currentThread().getId());
 		calculator = new Calculator();
