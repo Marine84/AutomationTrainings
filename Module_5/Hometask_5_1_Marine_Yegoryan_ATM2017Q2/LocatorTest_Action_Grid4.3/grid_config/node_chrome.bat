e6090e5992bd7ab8f384387bdfaf2a7c04958dbb
marineyegoryan@gmail.com <marine_yegoryan@epam.com>
2017-07-10 15:55:59 +0400

    updated grid functionality

--- Module_5/Hometask_5_1_Marine_Yegoryan_ATM2017Q2/LocatorTest_Action_Grid4.3/grid_config/node_chrome.bat
+++ Module_5/Hometask_5_1_Marine_Yegoryan_ATM2017Q2/LocatorTest_Action_Grid4.3/grid_config/node_chrome.bat
@@ -1,1 +1,1 @@
-java -jar selenium-server-standalone-3.4.0.jar -role node -hub http://127.0.0.1:4444/grid/register -port 5555
+java -Dwebdriver.chrome.driver="C:\Users\marine_yegoryan\workspace\AutomationTrainings\Module_5\Hometask_5_1_Marine_Yegoryan_ATM2017Q2\LocatorTest_Action_Grid4.3\grid_config\chromedriver_linux64\chromedriver.exe" -jar selenium-server-standalone-3.4.0.jar -role node -browser browserName=chrome -hub http://127.0.0.1:4444/grid/register
