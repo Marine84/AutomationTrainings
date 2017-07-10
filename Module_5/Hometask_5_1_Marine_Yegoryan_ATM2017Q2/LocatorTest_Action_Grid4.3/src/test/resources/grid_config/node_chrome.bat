e611b3f8b69e38f36d1a0af036b12191d32b0a3c
marineyegoryan@gmail.com <marine_yegoryan@epam.com>
2017-07-10 16:04:12 +0400

    rename project name

--- Module_5/Hometask_5_1_Marine_Yegoryan_ATM2017Q2/LocatorTest_Action_Grid4.3/src/test/resources/grid_config/node_chrome.bat
+++
@@ -1,1 +0,0 @@
-java -Dwebdriver.chrome.driver="..\src\test\resources\grid_config\chromedriver_linux64\chromedriver.exe" -jar selenium-server-standalone-3.4.0.jar -role node -browser browserName=chrome -hub http://127.0.0.1:4444/grid/register
