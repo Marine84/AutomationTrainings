a87baeddbd7ee4dc71f897806e7f958032ebbbc8
marineyegoryan@gmail.com <marine_yegoryan@epam.com>
2017-07-31 18:20:26 +0400

    Added Builder pattern implementation

--- Module_5/Hometask_5_2_Marine_Yegoryan_ATM2017Q2/Design_Patterns5.2/src/test/java/pages/InputUsernamePage.java
+++ Module_5/Hometask_5_2_Marine_Yegoryan_ATM2017Q2/Design_Patterns5.2/src/test/java/pages/InputUsernamePage.java
@@ -1,6 +1,5 @@
 package pages;
 
-import business_objects.User;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
--- Module_5/Hometask_5_2_Marine_Yegoryan_ATM2017Q2/Design_Patterns5.2/src/test/java/tests/LoginTest.java
+++ Module_5/Hometask_5_2_Marine_Yegoryan_ATM2017Q2/Design_Patterns5.2/src/test/java/tests/LoginTest.java
@@ -1,21 +1,20 @@
 package tests;
 
-import Decorator.UserDecorator;
-import business_objects.User;
+import businessobjects_Builder.User;
 import org.openqa.selenium.WebDriver;
-import pages.AbstractPage;
 import pages.InputPasswordPage;
 import pages.InputUsernamePage;
-import org.openqa.selenium.By;
-import org.testng.Assert;
 import org.testng.annotations.*;
 import utils.WebDriverSingleton;
 
 public class LoginTest {
-
     protected WebDriver driver;
+    //   UserName and Pass are used either via decorator or build patterns
+    //   Get username and Pass via Build pattern
+    User user = new User.UserBuilder("marineyegoryan0884", "marinetest").build();
 
-    public UserDecorator user = new UserDecorator("marineyegoryan0884", "marinetest");
+    //  Get username and Pass via Decorator pattern
+    //   public UserDecorator user = new UserDecorator("marineyegoryan0884", "marinetest");
 
     @BeforeTest()
     @Parameters("baseUrl")
@@ -26,8 +25,8 @@ public class LoginTest {
         // Verify, that the login is successful
 //        Assert.assertEquals("Gmail", driver.findElement(By.xpath("//div[@aria-label='Navigate to']/span[text()='Gmail']")));
     }
+
     @AfterTest()
-//    @Parameters("baseUrl")
     public void logoutTest() {
         InputPasswordPage emailLogin = new InputPasswordPage().signOff();
 //        Assert.assertEquals("Gmail", driver.getTitle());
